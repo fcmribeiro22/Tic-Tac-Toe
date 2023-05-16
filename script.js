d64719d3abfcdcf1a87926f599cf935364048716
Fábio Ribeiro <fcmribeiro22@gmail.com>
2023-05-16 15:21:41 +0100

    Append event listeners to created cells

--- script.js
+++ script.js
@@ -2,39 +2,57 @@ const Gameboard = (() => {
   let board = ['', '', '', '', '', '', '', '', ''];
 
   const renderBoard = () => {
-    let boardLength= board.length;
-    let tempDiv;
-    for (i=0; i < boardLength; i++){
-      tempDiv =document.createElement ('div');
-      tempDiv.className ='cell';
-      tempDiv.innerHTML =board[i];
-      document.getElementById('gameboard').appendChild(tempDiv);
-      console.log(boardLength);
+    let boardCell;
+    for (let i=0; i < board.length; i++){
+      boardCell =document.createElement ('div');
+      boardCell.className ='cell';
+      boardCell.innerHTML =board[i];
+      document.getElementById('gameboard').appendChild(boardCell);
+      console.log(board.length);
     }
   }
 
     const resetBoard = () => {
       board = ['', '', '', '', '', '', '', '', ''];
+      renderBoard()
+    };
+
+    const updateBoard = () => {
+      let playedCell = document.querySelectorAll('.cell');
+      console.log(playedCell);
+      for (let i=0 ; i < playedCell.length ; i++){
+        playedCell[i].addEventListener('click', () =>{
+          if (playedCell[i].innerHTML === ""){
+            playedCell[i].innerHTML = Player2.getMarker();
+            
+          } 
+          
+        })
+      }
     };
   
   return {
     resetBoard, 
     renderBoard,
+    updateBoard
   };
 })();
 
 
 
-const Player = (name, symbol) => {
+const Player = (name, marker) => {
   const getName = () => name;
-  const getSymbol = () => symbol;
+  const getMarker = () => marker;
 
   return {
     getName,
-    getSymbol,
+    getMarker,
   };
 };
 
-
+const Player1 = Player('Adam', 'X')
+const Player2= Player('Sandler', 'O')
 
 Gameboard.renderBoard();
+
+Gameboard.updateBoard();
